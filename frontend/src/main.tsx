c4d6327216c1648252bc669484dcdea9abbfc1bb
resarytrew <resarytrew@gmail.com>
2026-07-26 11:19:24 +0500

    feat: bootstrap web app through hub gate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { HubConnectionGate, HubProvider } from "./hub/HubProvider";
 import "./styles.css";
+
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    void navigator.serviceWorker.register("/sw.js");
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <HubProvider>
+      <HubConnectionGate>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </HubConnectionGate>
+    </HubProvider>
   </React.StrictMode>,
 );
